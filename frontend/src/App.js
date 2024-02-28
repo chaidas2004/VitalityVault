018b3a4419bac4d44c9bf774f761909b9e5f4c1d
ehalle0 <159745366+ehalle0@users.noreply.github.com>
2024-02-28 12:05:26 -0800

    Update App.js to import firebase and some firebase functions for login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,29 @@ import React from 'react';
 import './App.css';
 import './Dashboard.css';
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import {getAuth, sighnOut, createUserWithEmailAndPassword, signInWithEmailAndPassword} from "@firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyB2wHRLmXZuLvkZS7GPNxDteyyfmDcerrY",
+  authDomain: "vitality-vault.firebaseapp.com",
+  projectId: "vitality-vault",
+  storageBucket: "vitality-vault.appspot.com",
+  messagingSenderId: "941191796132",
+  appId: "1:941191796132:web:c77c0d590551ba4d909d31",
+  measurementId: "G-QE6LQ40VL2"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 function App() {
 return (
   <div className="App">
