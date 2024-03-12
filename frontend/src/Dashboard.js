18f9ad59bae221a043e70bfc4371f2a0f76f21cc
Chai Das <chaidas04@gmail.com>
2024-03-11 17:33:13 -0700

    Minor adjustment to UI font and nav bar (color change and postioning)

--- frontend/src/Dashboard.js
+++ frontend/src/Dashboard.js
@@ -1,24 +1,17 @@
-import React, { useState } from 'react';
+
+import React from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { signOut } from 'firebase/auth';
-import { auth } from './config/firebase'
-import { useUser } from './AuthContext'
+import { auth } from './config/firebase';
+import { useUser } from './AuthContext';
 import './Dashboard.css';
 import StopWatch from './StopWatch';
-import Login from './Login'; 
+import video from './video/VV.mp4';
 
 const Dashboard = () => {
   const { currentUser } = useUser();
   const navigate = useNavigate();
-  const [showUserInformation, setShowUserInformation] = useState(false);
 
-  const toggleUserInformation = () => {
-    setShowUserInformation(prevState => !prevState);
-  };
-
-  const closeUserInformation = () => {
-    setShowUserInformation(false);
-  };
 
   const logOut = async () => {
     try {
@@ -28,6 +21,7 @@ const Dashboard = () => {
       console.error(err);
     }
   };
+
   return (
     <div className="dashboard">
       <header className="main-header">
@@ -41,20 +35,12 @@ const Dashboard = () => {
           <div className="button-container">
             <p>Hello, {currentUser.displayName || "User"}</p>
             <button onClick={logOut}>Log Out</button>
-            <redirect to="/" />
-      </div>
+          </div>
         </div>
       </header>
 
-      <div className={`half-page ${showUserInformation ? 'slide-in' : 'slide-out'}`}>
-        <div className="content">
-          <h2>Workout Summary</h2>
-          {}
-          {}
-          <button className="close-button" onClick={closeUserInformation}>Close</button>
-        </div>
-        <StopWatch />
-      </div>
+      <video src={video} width="100%" controls autoPlay loop />
+      <StopWatch />
     </div>
   );
 };
