a3c63567aad43907b1355763c46115a88293c4a1
Mingyu Choi <mcmg98@g.ucla.edu>
2024-03-12 18:57:46 -0700

    Modified the search feature so that data can be searched by either inputting name or tag

--- frontend/src/WorkoutSearch.js
+++ frontend/src/WorkoutSearch.js
@@ -5,8 +5,7 @@ import { collection, query, where, getDocs } from "firebase/firestore";
 const WorkoutSearch = () => {
   const [workouts, setWorkouts] = useState([]);
   const [name, setName] = useState('');
-  const [duration, setDuration] = useState('');
-  const [category, setCategory] = useState('');
+  const [tags, setTags] = useState('');
 
   const handleSearch = async () => {
     let q = query(collection(db, "workouts"));
@@ -14,11 +13,8 @@ const WorkoutSearch = () => {
     if (name !== '') {
       q = query(q, where("name", "==", name));
     }
-    if (duration !== '') {
-      q = query(q, where("duration", "==", parseInt(duration)));
-    }
-    if (category !== '') {
-      q = query(q, where("category", "==", category));
+    if (tags !== '') {
+      q = query(q, where("tags", "==", tags));
     }
 
     const querySnapshot = await getDocs(q);
@@ -35,29 +31,16 @@ const WorkoutSearch = () => {
   
         <label htmlFor="name">Name:</label>
         <input type="text" id="name" value={name} onChange={(e) => setName(e.target.value)} /><br /><br />
+
+        <label htmlFor="tags">Tags:</label>
+        <input type="text" id="tags" value={tags} onChange={(e) => setTags(e.target.value)} /><br /><br />        
   
-        <label htmlFor="duration">Duration:</label>
-        <select id="duration" value={duration} onChange={(e) => setDuration(e.target.value)}>
-          <option value="">Select Duration</option>
-          <option value="5">5 Minutes</option>
-          <option value="10">10 Minutes</option>
-          <option value="20">20 Minutes</option>
-          <option value="30">30 Minutes</option>
-          <option value="45">45 Minutes</option>
-          <option value="60">60 Minutes</option>
-        </select><br /><br />
-  
-        <label htmlFor="category">Category:</label>
-        <select id="category" value={category} onChange={(e) => setCategory(e.target.value)}>
-          <option value="">Select Category</option>
-          <option value="strength">Strength</option>
-          <option value="cardio">Cardio</option>
-        </select><br /><br />
         <button onClick={handleSearch}>Search</button>
+  
         <div>
           {workouts.map((workout) => (
             <div key={workout.id}>
-              <p>Name: {workout.name}, Duration: {workout.duration} minutes, Category: {workout.category}</p>
+              <p>Name: {workout.name}, , Tags: {workout.tags}</p>
             </div>
           ))}
         </div>
